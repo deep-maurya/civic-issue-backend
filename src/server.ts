839007caf2f252c02b7bead447add0b4088e011d
palak-29 <121228830+palak-29@users.noreply.github.com>
2025-10-25 20:23:49 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,25 @@ import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 import routes from './routes';
+import fastifyMulter from 'fastify-multer';
+import path from 'path';
+import fastifyStatic from '@fastify/static';
 
 dotenv.config();
 
 const startServer = async () => {
-  // Create Fastify instance
   const app = Fastify({ logger: true });
 
   // Register CORS
-  await app.register(cors, {
-    origin: '*',
+  await app.register(cors, { origin: '*' });
+
+  // Register Multer parser
+  await app.register(fastifyMulter.contentParser);
+
+  // Serve static /uploads folder
+  app.register(fastifyStatic, {
+    root: path.join(__dirname, 'uploads'),
+    prefix: '/uploads/', // accessible as /uploads/<filename>
   });
 
   // Connect to MongoDB
@@ -25,7 +34,6 @@ const startServer = async () => {
     return { message: '🚀 Fastify server running!' };
   });
 
-  // Start server
   const PORT = Number(process.env.PORT) || 5000;
   await app.listen({ port: PORT, host: '0.0.0.0' });
   console.log(`Server running on http://localhost:${PORT}`);
